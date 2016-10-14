fd28d9cc13b2dcbc3142691d3232a9212b6962e4
Benjamin Cutilli <benvcutilli@gmail.com>
2016-10-14 00:50:09 -0400

    good place to stop for now

--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,74 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from shared.languageLocalization import baseLocalization
 
+debugLocale = 'english'
+godMode = True
+
+######### LOGIN LOCALIZATION ##########
+loginFrenchDict = {
+    'loginLocal': 'Connexion',
+    'usernameLocal': 'nom d\'utiliser',
+    'passwordLocal': 'mot de passe',
+    'logInButtonLocal': 'Allons y!',
+    'newUserLocal': 'Je n\'ai jamais été ici avant de ce moment',
+    'forgotPasswordLocal': 'Alors...j\'ai oublié mon mot de passe',
+}
+loginFrenchDict.update(baseLocalization['french'])
+
+loginEnglishDict = {
+    'loginLocal': 'Login',
+    'usernameLocal': 'username',
+    'passwordLocal': 'password',
+    'logInButtonLocal': 'Let\'s go!',
+    'newUserLocal': 'I\'m new',
+    'forgotPasswordLocal': 'Well...I forgot my password',
+}
+loginEnglishDict.update(baseLocalization['english'])
+
+loginLocalization = {
+    'french'    :   loginFrenchDict,
+    'english'   :   loginEnglishDict,
+}
+######### END OF LOCALIZATION ##########
+
+######### HOMEPAGE LOCALIZATION ########
+
+homepageEnglishDict = {
+    'updatesTitle'  :   'Updates',
+    'followingTitle':   'Following',
+    'noPosts'       :   'No posts :(',
+    'encouragement' :   ' Follow some people!'
+}
+homepageEnglishDict.update(baseLocalization['english'])
+
+homepageFrenchDict = {
+    'updatesTitle'  :   'Actualités',
+    'followingTitle':   'Abonnements',
+    'noPosts'       :   'Il n\'y a pas des actualités :(',
+    'encouragement' :   'Abonnez-vous à des autres personnes!'
+}
+homepageFrenchDict.update(baseLocalization['french'])
+
+homepageLocalization = {
+    'french'    :   homepageFrenchDict,
+    'english'   :   homepageEnglishDict
+}
+
+######### END OF LOCALIZATION ##########
 def homepage(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated and not godMode:
         return loginView(request)
     else:
-        return render(request, 'homepage/homepage.html', {})
+        return render(request, 'homepage/homepage.html', homepageLocalization[debugLocale])
 
 
 def loginView(request):
-    return render(request, 'homepage/login.html', {})
+
+
+
+    if request.method == "POST":
+        return HttpResponseRedirect(reverse("homepage"))
+    else:
+        return render(request, 'homepage/login.html', loginLocalization[debugLocale])
---
+++ shared/languageLocalization.py
@@ -0,0 +1,20 @@
+englishLocalization = {
+    'newLog'    :   "w",
+    'about'     :   "i",
+    'logout'    :   "x",
+    'settings'  :   "s",
+
+}
+
+frenchLocalization = {
+    'newLog'    :   "é",
+    'about'     :   "à",
+    'logout'    :   "q",
+    'settings'  :   "r",
+}
+
+
+baseLocalization = {
+    'english'   :   englishLocalization,
+    'french'    :  frenchLocalization,
+}
