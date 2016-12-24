703e26394129fff4862c7695d182af9e554ea20c
Benjamin Cutilli <benvcutilli@gmail.com>
2016-12-23 23:01:25 -0500

    implemented shared localization for remaining templates (still more templates to be made, though)

--- settings/views.py
+++ settings/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from shared.languageLocalization import baseLocalization
 
 # Create your views here.
 
+baseLocale = 'french'
+
+settingsFrenchDict = {
+}
+settingsFrenchDict.update(baseLocalization['french'])
+
+settingsEnglishDict = {
+}
+settingsEnglishDict.update(baseLocalization['english'])
+
+localizationDict = {
+    'french'    :   settingsFrenchDict,
+    'english'   :  settingsEnglishDict,
+}
+
 def settings(request):
-    return render(request, "settings/settings.html", {})
+    return render(request, "settings/settings.html", localizationDict[baseLocale])
--- workoutLogging/views.py
+++ workoutLogging/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+from shared.languageLocalization import baseLocalization
+
+debugLocale = 'french'
+
+entryFrenchDict = {
+}
+entryFrenchDict.update(baseLocalization['french'])
+
+entryEnglishDict = {
+}
+entryEnglishDict.update(baseLocalization['english'])
+
+entryLocalization = {
+    'french'    :   entryFrenchDict,
+    'english'   :   entryEnglishDict,
+}
 
 # Create your views here.
 
 def newEntry(request):
-    return render(request, "workoutLogging/newentry.html", {})
+    return render(request, "workoutLogging/newentry.html", entryLocalization[debugLocale])
 
 def editEntry(request):
-    return render(request, "workoutLogging/editentry.html", {})
+    return render(request, "workoutLogging/editentry.html", entryLocalization[debugLocale])
 
 def viewEntry(request):
-    return render(request, "workoutLogging/viewentry.html", {})
+    return render(request, "workoutLogging/viewentry.html", entryLocalization[debugLocale])
