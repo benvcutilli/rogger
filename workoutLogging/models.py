8653d89ad1e97421c63c145a71d6a99d3812d72e
Benjamin Cutilli <benvcutilli@gmail.com>
2017-01-04 13:58:02 -0500

    models now migrated

--- workoutLogging/models.py
+++ workoutLogging/models.py
@@ -7,13 +7,13 @@ class Workout(models.Model):
     distance    =   models.FloatField()
     # next line citation [16]
     shoe        =   models.ForeignKey('settings.Shoe', on_delete=models.SET_NULL, null=True)
-    entry       =   models.TextField()
-    hours       =   models.FloatField()
-    minutes     =   models.FloatField()
-    seconds     =   models.FloatField()
+    entry       =   models.TextField(default="")
+    hours       =   models.FloatField(null=True)
+    minutes     =   models.FloatField(null=True)
+    seconds     =   models.FloatField(null=True)
     # type/subtype from Merv
-    wtype       =   models.CharField(max_length=50)
+    wtype       =   models.CharField(max_length=50, default="")
     wsubtype    =   models.CharField(max_length=50, null=True)
-    title       =   models.CharField(max_length=100)
+    title       =   models.CharField(max_length=100, default="")
 
-    owner       =   models.ForeignKey(User, on_delete=models.CASCADE)
+    owner       =   models.ForeignKey(User, on_delete=models.CASCADE, null=True)
