5f3448e2fdc1cb424a44c5c7bd84644484446a0f
Benjamin Cutilli <benvcutilli@gmail.com>
2017-01-06 22:33:14 -0500

    almost done with editing log entries

--- workoutLogging/urls.py
+++ workoutLogging/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from workoutLogging import views
 urlpatterns = [
     url(r'new$', views.newEntry, name="newEntryView"),
-    url(r'\d+$', views.editEntry, name="editEntryView"),
+    url(r'(\d+)$', views.editEntry, name="editEntryView"),
     url(r'view$', views.viewEntry, name="viewEntryView"),
 ]
--- workoutLogging/views.py
+++ workoutLogging/views.py
@@ -33,7 +33,7 @@ def newEntry(request):
         'types'     :   [(element, repr(element.id)) for element in availableWorkoutTypes]
     })
 
-    
+
 
     if request.user.is_authenticated:
         workoutForm = forms.WorkoutForm({
@@ -86,8 +86,63 @@ def newEntry(request):
     else:
         return HttpResponseForbidden()
 
-def editEntry(request):
-    return render(request, "workoutLogging/editentry.html", entryLocalization[debugLocale])
+def editEntry(request, workoutID):
+    templateDict = entryLocalization[debugLocale]
+    availableWorkoutTypes = WorkoutType.objects.filter(owner__isnull=True) | WorkoutType.objects.filter(owner=request.user)
+    templateDict.update({
+        'formURL'   :   reverse("editEntryView", args=[workoutID]),
+        'error'     :   "",
+        'shoes'     :   [(element, repr(element.id)) for element in Shoe.objects.filter(owner=request.user)],
+        'types'     :   [(element, repr(element.id)) for element in availableWorkoutTypes]
+    })
+    workout = Workout.objects.get(id=int(workoutID))
+    workoutForm = forms.WorkoutForm({
+        'title'     :   workout.title,
+        'distance'  :   repr(workout.distance),
+        'hours'     :   repr(workout.hours) if workout.hours != None else "",
+        'minutes'   :   repr(workout.minutes) if workout.minutes != None else "",
+        'seconds'   :   repr(workout.seconds) if workout.seconds != None else "",
+        'wtype'     :   repr(workout.wtype.id),
+        'shoe'      :   repr(workout.shoe.id) if workout.shoe != None else "-1",
+        'entry'     :   workout.entry,
+        'date'      :   repr(workout.date.year) + "." + ("0" if workout.date.month < 10 else "") + repr(workout.date.month) + "." + ("0" if workout.date.day < 10 else "") + repr(workout.date.day),
+    })
+
+    if request.method == "POST":
+        workoutForm = forms.WorkoutForm({
+            'title'     :   request.POST['title'],
+            'distance'  :   request.POST['distance'],
+            'hours'     :   request.POST['hours'],
+            'minutes'   :   request.POST['minutes'],
+            'seconds'   :   request.POST['seconds'],
+            'wtype'     :   request.POST['type'],
+            'shoe'      :   request.POST['shoe'],
+            'entry'     :   request.POST['entryText'],
+            'date'      :   request.POST['date'],
+        })
+        if workoutForm.is_valid():
+            workout = Workout.objects.get(id=workoutID)
+
+            workout.title       =   workoutForm.cleaned_data['title'],
+            workout.distance    =   workoutForm.cleaned_data['distance'],
+            workout.hours       =   workoutForm.cleaned_data['hours'],
+            workout.minutes     =   workoutForm.cleaned_data['minutes'],
+            workout.seconds     =   workoutForm.cleaned_data['seconds'],
+            workout.wtype       =   WorkoutType.objects.get(id=int(workoutForm.cleaned_data['wtype'])),
+            workout.shoe        =   Shoe.objects.get(id=workoutForm.cleaned_data['shoe']) if workoutForm.cleaned_data['shoe'] >= 0 else None,
+            workout.entry       =   workoutForm.cleaned_data['entry'],
+            workout.owner       =   request.user,
+            workout.date        =   workoutForm.cleaned_data['date']
+            workout.updated     =   True
+
+
+            workout.save()
+            return HttpResponseRedirect(reverse("homepage"))
+
+    templateDict.update({
+        'form'  :   workoutForm
+    })
+    return render(request, "workoutLogging/editentry.html", templateDict)
 
 def viewEntry(request):
     return render(request, "workoutLogging/viewentry.html", entryLocalization[debugLocale])
