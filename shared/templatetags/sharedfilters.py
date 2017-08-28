6b94e78c9822eb8e57e8d4f7637eef4302544b25
Benjamin Cutilli <benvcutilli@gmail.com>
2017-08-28 11:49:34 -0400

    fixed the pretty decimal non-decimal point bug

--- shared/templatetags/sharedfilters.py
+++ shared/templatetags/sharedfilters.py
@@ -8,6 +8,8 @@ def prettydecimal(thing):
     stringThing = str(thing)
     if len(stringThing) == 0:
         return ""
+    if "." not in stringThing:
+        return stringThing
     index = len(stringThing) - 1
     while stringThing[index] == '0' and stringThing[index] != '.':
         index -= 1
