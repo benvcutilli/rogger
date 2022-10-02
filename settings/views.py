62304516dcfbb5d84af43455f07e4592e3fe96ce
Benjamin Cutilli <b@Benjamins-MacBook-Air.local>
2022-10-02 15:55:20 -0400

    removing broken code (hopefully)

--- settings/views.py
+++ settings/views.py
@@ -170,9 +170,8 @@ def settings(request):
         templateDict = {}
         templateDict.update(localizationDict[baseLocale])
         templateDict.update({
-            'shoes'           : Shoe.objects.filter(owner=request.user),
-            'workoutTypes'    : WorkoutType.objects.filter(owner=request.user).exclude(name="Unknown"),
-            'protectionLevel' : user.userInfo.privacySelection
+            'shoes'         : Shoe.objects.filter(owner=request.user),
+            'workoutTypes'  : WorkoutType.objects.filter(owner=request.user).exclude(name="Unknown")
         })
         return render(request, "settings/settings.html", templateDict)
     else:
